8506d228e57c97b9c27858e015039f5124daafd9
kirgo36@gmail.com <kirgo36@gmail.com>
2020-10-04 11:17:03 +0200

    thinking about potential optymizations

---
+++ Core01/WORK-observable/Sessions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using static LanguageExt.Prelude;
+
+namespace MarcinGajda.WORK_observable
+{
+    class Sessions
+    {
+        readonly ISubject<Session> session = Subject.Synchronize(new Subject<Session>());
+        static readonly TimeSpan interval = TimeSpan.FromSeconds(15);
+
+        public Sessions()
+        {
+            session
+                .Scan(ImmutableSortedSet.Create<Session>(Session.DateComparer), (set, next) => {
+                    var now = DateTimeOffset.UtcNow;
+                    return set.Except(set.TakeWhile(session => now - session.Date > interval));
+                });
+        }
+    }
+    class Session
+    {
+        public static readonly Comparer<Session> DateComparer = Comparer<Session>.Create(DateComparison);
+        private static int DateComparison(Session left, Session right) => left.Date.CompareTo(right.Date);
+
+        public DateTimeOffset Date { get; }
+    }
+}
