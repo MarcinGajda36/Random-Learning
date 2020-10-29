7b4c38b3db51e3dca679c1a8bf151a4c2970b440
kirgo36@gmail.com <kirgo36@gmail.com>
2020-10-29 18:08:45 +0100

    adding second SemaphoreWrapper focused on safety

--- Core01/LocksAndSemaphores/SemaphoreWrapper.cs
+++ Core01/LocksAndSemaphores/SemaphoreWrapper.cs
@@ -5,12 +5,53 @@ using System.Threading.Tasks;
 
 namespace MarcinGajda.LocksAndSemaphores
 {
-    public class SemaphoreWrapper : IDisposable
+    public class SafeSemaphoreWrapper : IDisposable
+    {
+        private readonly SemaphoreSlim semaphoreSlim;
+
+        public SafeSemaphoreWrapper(int initial = 1, int max = 1)
+            => semaphoreSlim = new SemaphoreSlim(initial, max);
+
+        public async Task<ReleaseOnDispose> EnterAsync(CancellationToken cancellationToken = default)
+        {
+            await semaphoreSlim.WaitAsync(cancellationToken);
+            return new ReleaseOnDispose(semaphoreSlim);
+        }
+        public ReleaseOnDispose Enter(CancellationToken cancellationToken = default)
+        {
+            semaphoreSlim.Wait(cancellationToken);
+            return new ReleaseOnDispose(semaphoreSlim);
+        }
+
+        public class ReleaseOnDispose : IDisposable
+        {
+            private SemaphoreSlim semaphoreSlim;
+
+            public ReleaseOnDispose(SemaphoreSlim semaphoreSlim)
+            {
+                this.semaphoreSlim = semaphoreSlim;
+            }
+
+            public void Dispose()
+                => Interlocked.Exchange(ref semaphoreSlim, null)?.Release();
+
+            ~ReleaseOnDispose()
+            {
+                Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        public void Dispose()
+            => semaphoreSlim.Dispose();
+    }
+
+    public class FastSemaphoreWrapper : IDisposable
     {
         private readonly SemaphoreSlim semaphoreSlim;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public SemaphoreWrapper(int initial = 1, int max = 1)
+        public FastSemaphoreWrapper(int initial = 1, int max = 1)
             => semaphoreSlim = new SemaphoreSlim(initial, max);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -19,65 +60,12 @@ namespace MarcinGajda.LocksAndSemaphores
             await semaphoreSlim.WaitAsync(cancellationToken);
             return new ReleaseOnDispose(semaphoreSlim);
         }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public RefReleaseOnDispose Enter(CancellationToken cancellationToken = default)
         {
             semaphoreSlim.Wait(cancellationToken);
             return new RefReleaseOnDispose(semaphoreSlim);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public async Task<TResult> EnterAsyncDoSync<TResult>(Func<TResult> toDo, CancellationToken cancellationToken = default)
-        {
-            using (await EnterAsync(cancellationToken))
-                return toDo();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public async Task<TResult> DoAsync<TResult>(Func<Task<TResult>> toDo, CancellationToken cancellationToken = default)
-        {
-            using (await EnterAsync(cancellationToken))
-                return await toDo();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public async Task<TResult> DoAsync<TResult>(Func<CancellationToken, Task<TResult>> toDo, CancellationToken cancellationToken = default)
-        {
-            using (await EnterAsync(cancellationToken))
-                return await toDo(cancellationToken);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public async Task EnterAsyncDoSync(Action<CancellationToken> toDo, CancellationToken cancellationToken = default)
-        {
-            using (await EnterAsync(cancellationToken))
-                toDo(cancellationToken);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public async Task EnterAsyncDoSync(Action toDo, CancellationToken cancellationToken = default)
-        {
-            using (await EnterAsync(cancellationToken))
-                toDo();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TResult DoSync<TResult>(Func<CancellationToken, TResult> toDo, CancellationToken cancellationToken = default)
-        {
-            using (Enter(cancellationToken))
-                return toDo(cancellationToken);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TResult DoSync<TResult>(Func<TResult> toDo, CancellationToken cancellationToken = default)
-        {
-            using (Enter(cancellationToken))
-                return toDo();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void DoSync(Action<CancellationToken> toDo, CancellationToken cancellationToken = default)
-        {
-            using (Enter(cancellationToken))
-                toDo(cancellationToken);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void DoSync(Action toDo, CancellationToken cancellationToken = default)
-        {
-            using (Enter(cancellationToken))
-                toDo();
         }
 
         public readonly struct ReleaseOnDispose : IDisposable
