dfb79e5ce75881ac645565c075ebaa2c4f1bb4f1
kirgo36@gmail.com <kirgo36@gmail.com>
2020-11-12 18:54:55 +0100

    trying 5.0 but it's not working

---
+++ 50Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _50Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var person = new Person("Bill", "Wagner");
+
+            //var (first, last) = person;
+            //Console.WriteLine(first);
+            //Console.WriteLine(last);
+            //Person brother = person with { FirstName = "Paul" };
+        }
+
+    public record Person
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+
+        public Person(string first, string last) => (FirstName, LastName) = (first, last);
+    }
+}
+}
