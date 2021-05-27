8c84f52421bdd6055186bf231c7f4f2a0fd1202c
kirgo36@gmail.com <kirgo36@gmail.com>
2021-05-27 20:46:17 +0200

    trying out nuint

---
+++ TestHelp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestHelp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nuint x = 45;
+            x += 123;
+            if(x == 1231)
+            {
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
