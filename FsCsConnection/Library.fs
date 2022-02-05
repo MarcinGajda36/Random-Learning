a605c1eba5e79a227c589f5809c429738808c173
kirgo36@gmail.com <kirgo36@gmail.com>
2022-02-05 09:58:21 +0100

    testing inref

--- FsCsConnection/Library.fs
+++ FsCsConnection/Library.fs
@@ -1,6 +1,14 @@
 ﻿namespace FsCsConnection
 
 module Say =
+    let methodWithIn (a:inref<int>) =
+        a + 10
+
+    let useInRef =
+        let x = 1
+        methodWithIn &x
+
+
     let hello name =
         printfn "Hello %s" name
 
