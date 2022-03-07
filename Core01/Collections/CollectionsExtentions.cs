24524b6e351a6266fb9ff95b9059a809cef7b0f4
kirgo36@gmail.com <kirgo36@gmail.com>
2022-03-07 16:36:43 +0100

    adding ForEachAsync

--- Core01/Collections/CollectionsExtentions.cs
+++ Core01/Collections/CollectionsExtentions.cs
@@ -1,30 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
+using System.Threading.Tasks;
 
-namespace MarcinGajda.Collections
+namespace MarcinGajda.Collections;
+
+public static class CollectionsExtentions
 {
-    public static class CollectionsExtentions
-    {
-        public static TResult[] Map<TElement, TResult>(this TElement[] array, Func<TElement, TResult> mapper)
-        {
-            var arr = new TResult[array.Length];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = mapper(array[i]);
-            }
-            return arr;
-        }
-        public static void Test()
-        {
-            IEnumerable<IEnumerable<int>> many = Enumerable.Range(0, 10).Select(i => Enumerable.Range(0, i));
-            var flatten =
-                from collection in many
-                from element in collection
-                select element;
+    public static Task<IReadOnlyCollection<Exception>> ForEachAsync<TElement>(
+        this IEnumerable<TElement> elements,
+        Func<TElement, Task> func)
+        => ForEachAsync(elements, func, Environment.ProcessorCount);
 
+    public static async Task<IReadOnlyCollection<Exception>> ForEachAsync<TElement>(
+        this IEnumerable<TElement> elements,
+        Func<TElement, Task> func,
+        int maxDegreeOfParallelism)
+    {
+        var exceptions = new List<Exception>();
+        static void AddException(List<Exception> exceptions, Task completed)
+        {
+            if (completed.IsFaulted)
+            {
+                exceptions.Add(completed.Exception!);
+            }
         }
+
+        var activeTasks = new LinkedList<Task>();
+        foreach (var element in elements)
+        {
+            if (activeTasks.Count < maxDegreeOfParallelism)
+            {
+                activeTasks.AddLast(func(element));
+            }
+            else
+            {
+                var completed = await Task.WhenAny(activeTasks);
+                AddException(exceptions, completed);
+                activeTasks.Remove(completed);
+                activeTasks.AddLast(func(element));
+            }
+        }
+
+        while (activeTasks.Count > 0)
+        {
+            var completed = await Task.WhenAny(activeTasks);
+            AddException(exceptions, completed);
+            activeTasks.Remove(completed);
+        }
+
+        return exceptions;
+    }
+
+    public static void Test()
+    {
+        var many = Enumerable.Range(0, 10).Select(i => Enumerable.Range(0, i));
+        var flatten =
+            from collection in many
+            from element in collection
+            select element;
+
     }
 }
