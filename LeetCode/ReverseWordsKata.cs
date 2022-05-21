0bb0e9bfba0e4ddc54f748a7db1e9846bc010454
kirgo36@gmail.com <kirgo36@gmail.com>
2022-05-21 19:53:57 +0200

    adding source

--- LeetCode/ReverseWordsKata.cs
+++ LeetCode/ReverseWordsKata.cs
@@ -3,6 +3,7 @@ using System.Linq;
 
 namespace LeetCode
 {
+    //https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/solutions/rust
     public static class ReverseWordsKata
     {
         public static string ReverseWords(string text)
