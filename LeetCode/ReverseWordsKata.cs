be6352b5a663ede689d2688514d0140a091de414
kirgo36@gmail.com <kirgo36@gmail.com>
2022-05-21 19:52:41 +0200

    quick Codewars Kata

---
+++ LeetCode/ReverseWordsKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public static class ReverseWordsKata
+    {
+        public static string ReverseWords(string text)
+        {
+            const string space = " ";
+            var words = text.Split(space);
+            var reversedWords = words.Select(word => string.Concat(word.Reverse()));
+            return string.Join(space, reversedWords);
+        }
+    }
+}
