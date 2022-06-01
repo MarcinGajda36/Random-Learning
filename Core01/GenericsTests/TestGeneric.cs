891589b21384662e6cf6b4effdf88143781c5cbe
kirgo36@gmail.com <kirgo36@gmail.com>
2022-06-01 08:51:00 +0200

    checking out anonymous delegate

--- Core01/GenericsTests/TestGeneric.cs
+++ Core01/GenericsTests/TestGeneric.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace MarcinGajda.GenericsTests
 {
@@ -7,30 +6,23 @@ namespace MarcinGajda.GenericsTests
     public static class TestGeneric
     {
         public static void TestEnum<T>(T t, T flag)
-            where T : Enum
-        {
-            t.HasFlag(flag);
-        }
+            where T : Enum => t.HasFlag(flag);
 
         public static void TestDelegate<TDelegate>(TDelegate @delegate)
             where TDelegate : Delegate
-            //where TDelegate : Map<int, string>
+            // where TDelegate : Map<int, string>
         {
             Map<int, string> map = new Map<int, string>((i) => "");
             Map<int, string> map1 = (i) => "";
             Map<int, string> map2 = TestMap;
             Func<int, string> f1 = TestMap;
 
+            var func1 = static int (in int x) => x;
+            TestDelegate(func1);
             //var mapped = @delegate.Map<int, string>(i => "");
         }
-        public static string TestMap(int i)
-        {
-            return i.ToString();
-        }
-        public static void TestTestDelegate()
-        {
-            TestDelegate(new Func<int>(() => 2));
-        }
+        public static string TestMap(int i) => i.ToString();
+        public static void TestTestDelegate() => TestDelegate(new Func<int>(() => 2));
 
     }
 }
