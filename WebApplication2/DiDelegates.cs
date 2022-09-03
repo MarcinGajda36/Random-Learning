9127614cd89064868fcb0882a67a3f2bcc8a811c
kirgo36@gmail.com <kirgo36@gmail.com>
2022-09-03 16:33:15 +0200

    playing with di

---
+++ WebApplication2/DiDelegates.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2;
+
+public delegate TClient ClientFactory<out TClient>(HttpClient httpClient);
+
+public class Client<TClient>
+{
+    private readonly ClientFactory<TClient> clientFactory;
+    private readonly IHttpClientFactory httpClientFactory;
+
+    public Client(ClientFactory<TClient> clientFactory, IHttpClientFactory httpClientFactory)
+    {
+        this.clientFactory = clientFactory;
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<TResult> UsingClient<TResult>(Func<TClient, Task<TResult>> use)
+    {
+        using var httpClient = httpClientFactory.CreateClient();
+        var client = clientFactory(httpClient);
+        return await use(client);
+    }
+}
---
+++ WebApplication2/Program.cs
@@ -0,0 +1,34 @@
+namespace WebApplication2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        _ = builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _ = builder.Services.AddEndpointsApiExplorer();
+        _ = builder.Services.AddSwaggerGen();
+        _ = builder.Services.AddSingleton<ClientFactory<object>>(provider => client => new object());
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            _ = app.UseSwagger();
+            _ = app.UseSwaggerUI();
+        }
+
+        _ = app.UseHttpsRedirection();
+
+        _ = app.UseAuthorization();
+
+        _ = app.MapControllers();
+
+        app.Run();
+    }
+}
