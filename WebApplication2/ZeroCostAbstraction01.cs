b138074f25575ebeb580c10eba1b6234910f48c5
kirgo36@gmail.com <kirgo36@gmail.com>
2022-11-01 14:29:24 +0100

    Trying zero cost abstractions

---
+++ WebApplication2/ZeroCostAbstraction01.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication2;
+
+public interface IClock { DateTimeOffset UtcNow(); }
+public struct Clock : IClock
+{
+    public DateTimeOffset UtcNow() => DateTimeOffset.UtcNow;
+}
+
+public interface IValidator<T> { bool IsValid(T value); }
+public record SomethingWithDate(DateTimeOffset SomeDate);
+public class DateValidator<TClock> : IValidator<SomethingWithDate>
+    where TClock : struct, IClock
+{
+    public bool IsValid(SomethingWithDate value)
+        => default(TClock).UtcNow().Date > value.SomeDate;
+}
+
