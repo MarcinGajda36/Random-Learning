6ad4d7e98ba83bf4d0984201fff2fb6bb4d22e06
kirgo36@gmail.com <kirgo36@gmail.com>
2022-11-12 23:23:30 +0100

    ExtentionsPlayground

---
+++ Core01/RandomGenerics/ExtentionsPlayground.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+namespace MarcinGajda.RandomGenerics;
+internal static class ExtentionsPlayground
+{
+    public static int InInt(this in int x)
+        => x;
+
+    public static ref int RefInt(this ref int x)
+        => ref x;
+
+    public static T VolatileRead<T>(this T t)
+        where T : class
+        => Volatile.Read(ref t);
+
+    public static int IDontKnow()
+    {
+        _ = 1.InInt();
+        int x = 1;
+        return x.RefInt();
+    }
+}
