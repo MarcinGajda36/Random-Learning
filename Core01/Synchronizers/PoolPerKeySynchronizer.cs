6c112ce08b958c2774e44e97ffaadd9ee7d2fac2
kirgo36@gmail.com <kirgo36@gmail.com>
2022-11-26 15:05:24 +0100

    adding more methods to PoolPerKeySynchronizer

--- Core01/Synchronizers/PoolPerKeySynchronizer.cs
+++ Core01/Synchronizers/PoolPerKeySynchronizer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MarcinGajda.Synchronizers;
 
-public sealed class PoolPerKeySynchronizer<TKey> : IDisposable
+public sealed class PoolPerKeySynchronizer<TKey>
+    : IDisposable
     where TKey : notnull
 {
     private readonly SemaphoreSlim[] pool;
@@ -30,7 +32,7 @@ public sealed class PoolPerKeySynchronizer<TKey> : IDisposable
         Func<TKey, TArgument, CancellationToken, Task<TResult>> resultFactory,
         CancellationToken cancellationToken = default)
     {
-        long index = (uint)key.GetHashCode() % pool.Length;
+        long index = GetIndex(key);
         var semaphore = pool[index];
         await semaphore.WaitAsync(cancellationToken);
         try
@@ -52,6 +54,93 @@ public sealed class PoolPerKeySynchronizer<TKey> : IDisposable
             resultFactory,
             static (key, factory, cancellation) => factory(key, cancellation),
             cancellationToken);
+
+    public async Task<TResult> SynchronizeAllAsync<TAgument, TResult>(
+        TAgument argument,
+        Func<TAgument, CancellationToken, Task<TResult>> resultFactory,
+        CancellationToken cancellationToken = default)
+    {
+        static void ReleaseAll(SemaphoreSlim[] pool, int index)
+        {
+            for (int toRelease = index; toRelease >= 0; toRelease--)
+            {
+                _ = pool[toRelease].Release();
+            }
+        }
+
+        int index = 0;
+        do
+        {
+            try
+            {
+                await pool[index].WaitAsync(cancellationToken);
+                index += 1;
+            }
+            catch
+            {
+                ReleaseAll(pool, index);
+                throw;
+            }
+        } while (index < pool.Length - 1);
+
+        try
+        {
+            return await resultFactory(argument, cancellationToken);
+        }
+        finally
+        {
+            ReleaseAll(pool, index);
+        }
+    }
+
+    public async Task<TResult> SynchronizeManyAsync<TArgument, TResult>(
+        IEnumerable<TKey> keys,
+        TArgument argument,
+        Func<TArgument, CancellationToken, Task<TResult>> resultFactory,
+        CancellationToken cancellationToken = default)
+    {
+        static void ReleaseLocked(SemaphoreSlim[] pool, Stack<long> locked)
+        {
+            while (locked.TryPop(out long toRelease))
+            {
+                _ = pool[toRelease].Release();
+            }
+        }
+
+        var indexes = new SortedSet<long>();
+        foreach (var key in keys)
+        {
+            long index = GetIndex(key);
+            _ = indexes.Add(index);
+        }
+
+        var locked = new Stack<long>(indexes.Count);
+        foreach (long toLock in indexes)
+        {
+            try
+            {
+                await pool[toLock].WaitAsync(cancellationToken);
+                locked.Push(toLock);
+            }
+            catch
+            {
+                ReleaseLocked(pool, locked);
+                throw;
+            }
+        }
+
+        try
+        {
+            return await resultFactory(argument, cancellationToken);
+        }
+        finally
+        {
+            ReleaseLocked(pool, locked);
+        }
+    }
+
+    private long GetIndex(TKey key)
+        => (uint)key.GetHashCode() % pool.Length;
 
     private void Dispose(bool disposing)
     {
