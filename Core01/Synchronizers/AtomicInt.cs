60b9877c51df7cde51eb5b3a14137a3539a4c818
kirgo36@gmail.com <kirgo36@gmail.com>
2022-12-03 19:59:01 +0100

    AtomicInt

---
+++ Core01/Synchronizers/AtomicInt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace MarcinGajda.Synchronizers;
+public sealed class AtomicInt
+{
+    private int value;
+    public int Value => Volatile.Read(ref value);
+    public AtomicInt(int initial = 0) => value = initial;
+
+    public int Swap<TArgument>(TArgument argument, Func<int, TArgument, int> swapper)
+    {
+        while (true)
+        {
+            int initial = Value;
+            int swapped = swapper(initial, argument);
+            if (Interlocked.CompareExchange(ref value, swapped, initial) == initial)
+            {
+                return swapped;
+            }
+        }
+    }
+
+    public int Swap(Func<int, int> swapper)
+        => Swap(swapper, static (initial, func) => func(initial));
+}
