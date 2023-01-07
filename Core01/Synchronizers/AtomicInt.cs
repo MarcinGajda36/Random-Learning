bb4eca67d33abf830fe7b723a3eed501d731b208
kirgo36@gmail.com <kirgo36@gmail.com>
2023-01-07 20:06:29 +0100

    adding pessimistic atomicInt

--- Core01/Synchronizers/AtomicInt.cs
+++ Core01/Synchronizers/AtomicInt.cs
@@ -2,11 +2,12 @@
 using System.Threading;
 
 namespace MarcinGajda.Synchronizers;
-public sealed class AtomicInt
+public sealed class OptymisticAtomicInt
 {
     private int value;
     public int Value => Volatile.Read(ref value);
-    public AtomicInt(int initial = 0) => value = initial;
+    public OptymisticAtomicInt(int initial = 0)
+        => value = initial;
 
     public int Swap<TArgument>(TArgument argument, Func<int, TArgument, int> swapper)
     {
@@ -23,4 +24,33 @@ public sealed class AtomicInt
 
     public int Swap(Func<int, int> swapper)
         => Swap(swapper, static (initial, func) => func(initial));
+}
+
+public sealed class PesimisticAtomicInt : IDisposable
+{
+    private readonly SemaphoreSlim semaphore = new(1, 1);
+    public int Value { get; private set; }
+
+    public PesimisticAtomicInt(int initial = 0)
+        => Value = initial;
+
+    public int Swap<TArgument>(TArgument argument, Func<int, TArgument, int> swapper)
+    {
+        semaphore.Wait();
+        try
+        {
+            Value = swapper(Value, argument);
+            return Value;
+        }
+        finally
+        {
+            _ = semaphore.Release();
+        }
+    }
+
+    public int Swap(Func<int, int> swapper)
+        => Swap(swapper, static (initial, func) => func(initial));
+
+    public void Dispose()
+        => semaphore.Dispose();
 }
