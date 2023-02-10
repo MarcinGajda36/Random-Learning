bb4e603135ccf157574d7408373748a08eec57e4
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-10 18:00:44 +0100

    HistoricalToLive clean

--- Core01/RXTests/HistoricalToLive.cs
+++ Core01/RXTests/HistoricalToLive.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reactive;
@@ -7,72 +8,59 @@ using System.Reactive.Linq;
 namespace MarcinGajda.RXTests;
 internal static class HistoricalToLive
 {
-    private interface IMessage
-    {
-    }
-
-    private sealed record EmptyMessage() : IMessage
-    {
-        public static readonly EmptyMessage Empty = new();
-    }
-
+    private interface IMessage { }
     private sealed record Live<TValue>(TValue Value) : IMessage;
     private sealed record Historical<TValue>(TValue Value) : IMessage;
     private sealed record HistoricalCompleted() : IMessage;
     private sealed record HistoricalError(Exception Exception) : IMessage;
 
-    private sealed record MessagesState<TValue>(
-        ImmutableList<TValue> LiveValues,
+    private sealed record ConcatState<TValue>(
+        ImmutableList<TValue> LiveBuffer,
         bool HasHistoricalEnded,
-        IMessage LastMessage)
+        IObservable<TValue> AvailableMessages,
+        IEqualityComparer<TValue>? Comparer)
     {
-        public static readonly MessagesState<TValue> Empty
-            = new(ImmutableList<TValue>.Empty, false, EmptyMessage.Empty);
+        public static ConcatState<TValue> Create(IEqualityComparer<TValue>? comparer)
+            => new(ImmutableList<TValue>.Empty, false, Observable.Empty<TValue>(), comparer);
     }
 
-    public static IObservable<TValue> Merge<TValue>(IObservable<TValue> live, IObservable<TValue> historical)
+    public static IObservable<TValue> ConcatLiveAfterHistory<TValue>(
+        IObservable<TValue> live,
+        IObservable<TValue> historical,
+        IEqualityComparer<TValue>? comparer = null)
         => GetLiveMessages(live)
         .Merge(GetHistoricalMessages(historical))
-        .Scan(MessagesState<TValue>.Empty, HandleNextMessage)
-        .Select(GetCurrentMessages)
+        .Scan(ConcatState<TValue>.Create(comparer), HandleNextMessage)
+        .Select(state => state.AvailableMessages)
         .Concat();
 
-    private static MessagesState<TValue> HandleNextMessage<TValue>(MessagesState<TValue> state, IMessage message)
+    private static ConcatState<TValue> HandleNextMessage<TValue>(ConcatState<TValue> state, IMessage message)
         => (state, message) switch
         {
-            ({ LastMessage: HistoricalCompleted }, Live<TValue> live) => state with { LastMessage = live, LiveValues = ImmutableList<TValue>.Empty },
-            ({ HasHistoricalEnded: true }, Live<TValue> live) => state with { LastMessage = live },
-            ({ HasHistoricalEnded: false }, Live<TValue> live) => state with { LastMessage = live, LiveValues = state.LiveValues.Add(live.Value) },
-            (_, Historical<TValue> historical) => state with { LastMessage = historical, LiveValues = state.LiveValues.Remove(historical.Value) },// TODO add comparer for 'state.LiveValues.Remove'?
-            (_, HistoricalCompleted endOfHistorical) => state with { LastMessage = endOfHistorical, HasHistoricalEnded = true },
-            (_, HistoricalError(var exception)) => throw exception,
-            _ => throw UnexpectedMergeState(nameof(HandleNextMessage), state, message)
+            ({ HasHistoricalEnded: true }, Live<TValue>(var live))
+                => state with { AvailableMessages = Observable.Return(live) },
+            ({ HasHistoricalEnded: false }, Live<TValue>(var live))
+                => state with { AvailableMessages = Observable.Empty<TValue>(), LiveBuffer = state.LiveBuffer.Add(live) },
+            (_, Historical<TValue>(var historical))
+                => state with { AvailableMessages = Observable.Return(historical), LiveBuffer = state.LiveBuffer.Remove(historical, state.Comparer) },
+            (_, HistoricalCompleted)
+                => state with { AvailableMessages = Observable.ToObservable(state.LiveBuffer), LiveBuffer = ImmutableList<TValue>.Empty, HasHistoricalEnded = true },
+            (_, HistoricalError(var exception))
+                => throw exception,
+            var unknown => throw new InvalidOperationException($"Unknown message state pair: '{unknown}'.")
         };
 
-    private static IObservable<TValue> GetCurrentMessages<TValue>(MessagesState<TValue> state)
-        => state switch
-        {
-            { HasHistoricalEnded: true, LastMessage: Live<TValue>(var live) } => Observable.Return(live),
-            { HasHistoricalEnded: false, LastMessage: Live<TValue> } => Observable.Empty<TValue>(),
-            { LastMessage: Historical<TValue>(var historical) } => Observable.Return(historical),
-            { LastMessage: HistoricalCompleted } => Observable.ToObservable(state.LiveValues),
-            _ => Observable.Throw<TValue>(UnexpectedMergeState(nameof(GetCurrentMessages), state, state.LastMessage))
-        };
+    private static IObservable<Live<TValue>> GetLiveMessages<TValue>(IObservable<TValue> live)
+        => live.Select(live => new Live<TValue>(live));
 
-    private static InvalidOperationException UnexpectedMergeState<TValue>(string place, MessagesState<TValue> state, IMessage message)
-        => throw new($"Unexpected '{place}' state inside: '{nameof(Merge)}', state: '{state}', message: '{message}'.");
-
-    private static IObservable<Live<TValue>> GetLiveMessages<TValue>(IObservable<TValue> liveValues)
-        => liveValues.Select(live => new Live<TValue>(live));
-
-    private static IObservable<IMessage> GetHistoricalMessages<TValue>(IObservable<TValue> historicalValues)
-        => historicalValues
+    private static IObservable<IMessage> GetHistoricalMessages<TValue>(IObservable<TValue> historical)
+        => historical
         .Materialize()
         .Select<Notification<TValue>, IMessage>(notification => notification switch
         {
             { Kind: NotificationKind.OnNext, Value: var value } => new Historical<TValue>(value),
             { Kind: NotificationKind.OnError, Exception: var exception } => new HistoricalError(exception),
             { Kind: NotificationKind.OnCompleted } => new HistoricalCompleted(),
-            _ => throw new NotSupportedException()
+            var unknown => throw new InvalidOperationException($"Unknown notification: '{unknown}'.")
         });
 }
