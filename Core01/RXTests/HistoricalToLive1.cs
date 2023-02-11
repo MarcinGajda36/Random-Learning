c16393b2d982d2140e43ddf5eb9a7624d9a2ae26
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-11 09:30:20 +0100

    adding not-de-duping HistoricalToLive and comment explaining de-dup danger

--- Core01/RXTests/HistoricalToLive1.cs
+++ Core01/RXTests/HistoricalToLive1.cs
@@ -6,7 +6,65 @@ using System.Reactive;
 using System.Reactive.Linq;
 
 namespace MarcinGajda.RXTests;
-internal static class HistoricalToLive1
+
+internal static class HistoricalToLive
+{
+    private interface IMessage { }
+    private sealed record Live<TValue>(TValue Value) : IMessage;
+    private sealed record Historical<TValue>(TValue Value) : IMessage;
+    private sealed record HistoricalCompleted() : IMessage;
+    private sealed record HistoricalError(Exception Exception) : IMessage;
+
+    private sealed record ConcatState<TValue>(
+        ImmutableQueue<TValue> LiveBuffer,
+        bool HasHistoricalEnded,
+        IObservable<TValue> AvailableMessages)
+    {
+        public static readonly ConcatState<TValue> Empty
+            = new(ImmutableQueue<TValue>.Empty, false, Observable.Empty<TValue>());
+    }
+
+    public static IObservable<TValue> ConcatLiveAfterHistory<TValue>(
+        IObservable<TValue> live,
+        IObservable<TValue> historical)
+        => GetLiveMessages(live)
+        .Merge(GetHistoricalMessages(historical))
+        .Scan(ConcatState<TValue>.Empty, HandleNextMessage)
+        .Select(state => state.AvailableMessages)
+        .Concat();
+
+    private static ConcatState<TValue> HandleNextMessage<TValue>(ConcatState<TValue> state, IMessage message)
+        => (state, message) switch
+        {
+            ({ HasHistoricalEnded: true }, Live<TValue>(var live))
+                => state with { AvailableMessages = Observable.Return(live) },
+            ({ HasHistoricalEnded: false }, Live<TValue>(var live))
+                => state with { AvailableMessages = Observable.Empty<TValue>(), LiveBuffer = state.LiveBuffer.Enqueue(live) },
+            (_, Historical<TValue>(var historical))
+                => state with { AvailableMessages = Observable.Return(historical) },
+            (_, HistoricalCompleted)
+                => state with { AvailableMessages = Observable.ToObservable(state.LiveBuffer), LiveBuffer = ImmutableQueue<TValue>.Empty, HasHistoricalEnded = true },
+            (_, HistoricalError(var exception))
+                => throw exception,
+            var unknown => throw new InvalidOperationException($"Unknown message state pair: '{unknown}'.")
+        };
+
+    private static IObservable<Live<TValue>> GetLiveMessages<TValue>(IObservable<TValue> live)
+        => live.Select(live => new Live<TValue>(live));
+
+    private static IObservable<IMessage> GetHistoricalMessages<TValue>(IObservable<TValue> historical)
+        => historical
+        .Materialize()
+        .Select<Notification<TValue>, IMessage>(notification => notification switch
+        {
+            { Kind: NotificationKind.OnNext, Value: var value } => new Historical<TValue>(value),
+            { Kind: NotificationKind.OnError, Exception: var exception } => new HistoricalError(exception),
+            { Kind: NotificationKind.OnCompleted } => new HistoricalCompleted(),
+            var unknown => throw new InvalidOperationException($"Unknown notification: '{unknown}'.")
+        });
+}
+
+internal static class HistoricalToLive1_Dedup
 {
     private interface IMessage { }
     private sealed record Live<TValue>(TValue Value) : IMessage;
@@ -19,6 +77,12 @@ internal static class HistoricalToLive1
         bool HasHistoricalEnded,
         IObservable<TValue> AvailableMessages,
         IEqualityComparer<TValue>? Comparer)
+    // Is comparer and de-duping needed?
+    // It can be harmful in scenario: 
+    // 1) Live returns Obj1_ver1 -> pushed to buffer
+    // 2) Live returns Obj1_ver2 -> pushed to buffer
+    // 3) History returns Obj1_ver2 -> Obj1_ver2 removed from buffer
+    // 4) User gets Historical(Obj1_ver2) Then older Live(Obj1_ver1)
     {
         public static ConcatState<TValue> Create(IEqualityComparer<TValue>? comparer)
             => new(ImmutableList<TValue>.Empty, false, Observable.Empty<TValue>(), comparer);
@@ -27,7 +91,7 @@ internal static class HistoricalToLive1
     public static IObservable<TValue> ConcatLiveAfterHistory<TValue>(
         IObservable<TValue> live,
         IObservable<TValue> historical,
-        IEqualityComparer<TValue>? comparer = null)// Is comparer and de-duping needed?
+        IEqualityComparer<TValue>? comparer = null)
         => GetLiveMessages(live)
         .Merge(GetHistoricalMessages(historical))
         .Scan(ConcatState<TValue>.Create(comparer), HandleNextMessage)
