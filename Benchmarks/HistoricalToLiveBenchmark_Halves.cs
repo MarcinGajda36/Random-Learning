52929d1362762d34315d10af8b69a5a78d538fee
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-12 14:37:24 +0100

    adding HistoricalToLiveBenchmark_Halves tests

---
+++ Benchmarks/HistoricalToLiveBenchmark_Halves.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using MarcinGajda.RXTests;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class HistoricalToLiveBenchmark_Halves
+{
+    private const int ElementsCount = 4096;
+
+    [Benchmark]
+    public void HistoricalToLive_UnionAndImmutable()
+    {
+        using var live = new Subject<int>();
+        using var historical = new Subject<int>();
+
+        using var merge = HistoricalToLive
+            .ConcatLiveAfterHistory(live, historical)
+            .Subscribe();
+
+        int half = ElementsCount / 2;
+        for (int i = 0; i < half; i++)
+        {
+            live.OnNext(i);
+        }
+        for (int i = 0; i < ElementsCount; i++)
+        {
+            historical.OnNext(i);
+        }
+        historical.OnCompleted();
+        for (int i = half; i < ElementsCount; i++)
+        {
+            live.OnNext(i);
+        }
+    }
+
+    [Benchmark]
+    public void HistoricalToLive2_StructAndMutation()
+    {
+        using var live = new Subject<int>();
+        using var historical = new Subject<int>();
+
+        using var merge = HistoricalToLive2
+            .ConcatLiveAfterHistory(live, historical)
+            .Subscribe();
+
+        int half = ElementsCount / 2;
+        for (int i = 0; i < half; i++)
+        {
+            live.OnNext(i);
+        }
+        for (int i = 0; i < ElementsCount; i++)
+        {
+            historical.OnNext(i);
+        }
+        historical.OnCompleted();
+        for (int i = half; i < ElementsCount; i++)
+        {
+            live.OnNext(i);
+        }
+    }
+}
--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -5,5 +5,5 @@ namespace Benchmarks;
 internal class Program
 {
     private static void Main(string[] args)
-        => BenchmarkRunner.Run<HistoricalToLiveBenchmark>();
+        => BenchmarkRunner.Run<HistoricalToLiveBenchmark_Halves>();
 }
