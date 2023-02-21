0f35c552c3b663cacdab2927b81b443fb088c3dd
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-21 09:29:57 +0100

    LockingPool

--- Core01/Collections/Pool.cs
+++ Core01/Collections/Pool.cs
@@ -36,7 +36,7 @@ class Pool<TValue> where TValue : class
     public Pool(int size, Func<TValue> factory)
     {
         this.factory = factory;
-        pool = new TValue[size];
+        pool = new TValue[size + 1];
     }
 
     public Lease Rent()
@@ -55,28 +55,108 @@ class Pool<TValue> where TValue : class
             }
             if (Interlocked.CompareExchange(ref rentIndex, nextToRent, toRent) == toRent)
             {
-                var value = pool[toRent]!;
+                var value = pool[toRent]!; // hmmm
                 pool[toRent] = null;
                 return new Lease(value, this);
             }
         }
     }
 
-    void Return(TValue toReturn) // TODO 
+    void Return(TValue value)
     {
         while (true)
         {
-            var current = returnIndex;
-            if (current == pool.Length - 1)
+            var toReturn = returnIndex;
+            var nextToReturn = toReturn + 1;
+            if (nextToReturn == pool.Length)
+            {
+                nextToReturn = 0;
+            }
+            if (nextToReturn == rentIndex)
             {
                 return;
             }
-            var next = current + 1;
-            if (Interlocked.CompareExchange(ref pool[current], toReturn, null) == null
-                && Interlocked.CompareExchange(ref returnIndex, next, current) == current)
+            if (Interlocked.CompareExchange(ref pool[toReturn], value, null) == null // Wrong, i can ruin some index forever with this,
+                                                                                     // maybe one int that return add rent subtracts?
+                && Interlocked.CompareExchange(ref returnIndex, nextToReturn, toReturn) == toReturn)
             {
                 return;
             }
+        }
+    }
+}
+class LockingPool<TValue>
+{
+    public struct Lease : IDisposable
+    {
+        readonly TValue value;
+        public TValue Value => isDisposed == AfterDispose
+            ? throw new ObjectDisposedException(nameof(Lease))
+            : value;
+
+        readonly LockingPool<TValue> parent;
+        const int AfterDispose = 1;
+        int isDisposed;
+
+        public Lease(TValue value, LockingPool<TValue> parent)
+        {
+            this.value = value;
+            this.parent = parent;
+        }
+
+        public void Dispose()
+        {
+            var temporary = Interlocked.Exchange(ref isDisposed, AfterDispose);
+            if (temporary != AfterDispose)
+            {
+                parent.Return(value);
+            }
+        }
+    }
+
+    readonly Func<TValue> factory;
+    readonly TValue[] pool;
+    volatile int available;
+
+    public LockingPool(int size, Func<TValue> factory)
+    {
+        this.factory = factory;
+        pool = new TValue[size];
+    }
+
+    public Lease Rent()
+    {
+        if (available == 0)
+        {
+            return new(factory(), this);
+        }
+
+        lock (pool)
+        {
+            if (available == 0)
+            {
+                return new(factory(), this);
+            }
+            available -= 1;
+            var toRent = pool[available];
+            return new(toRent, this);
+        }
+    }
+
+    void Return(TValue toReturn)
+    {
+        if (available == pool.Length - 1)
+        {
+            return;
+        }
+        lock (pool)
+        {
+            if (available == pool.Length - 1)
+            {
+                return;
+            }
+            pool[available] = toReturn;
+            available += 1;
         }
     }
 }
