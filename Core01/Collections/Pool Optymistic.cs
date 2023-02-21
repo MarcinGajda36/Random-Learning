d6b7e7f1558124f8049e5ddecdfbc7fe5e9763fe
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-21 13:12:04 +0100

    Playing with pooling

--- Core01/Collections/Pool Optymistic.cs
+++ Core01/Collections/Pool Optymistic.cs
@@ -76,87 +76,11 @@ class Pool<TValue> where TValue : class
             {
                 return;
             }
-            if (Interlocked.CompareExchange(ref pool[toReturn], value, null) == null // Wrong, i can ruin some index forever with this,
-                                                                                     // maybe one int that return add rent subtracts?
+            if (Interlocked.CompareExchange(ref pool[toReturn], value, null) == null // Is this fine?
                 && Interlocked.CompareExchange(ref returnIndex, nextToReturn, toReturn) == toReturn)
             {
                 return;
             }
-        }
-    }
-}
-class LockingPool<TValue>
-{
-    public struct Lease : IDisposable
-    {
-        readonly TValue value;
-        public TValue Value => isDisposed == AfterDispose
-            ? throw new ObjectDisposedException(nameof(Lease))
-            : value;
-
-        readonly LockingPool<TValue> parent;
-        const int AfterDispose = 1;
-        int isDisposed;
-
-        public Lease(TValue value, LockingPool<TValue> parent)
-        {
-            this.value = value;
-            this.parent = parent;
-        }
-
-        public void Dispose()
-        {
-            var temporary = Interlocked.Exchange(ref isDisposed, AfterDispose);
-            if (temporary != AfterDispose)
-            {
-                parent.Return(value);
-            }
-        }
-    }
-
-    readonly Func<TValue> factory;
-    readonly TValue[] pool;
-    volatile int available;
-
-    public LockingPool(int size, Func<TValue> factory)
-    {
-        this.factory = factory;
-        pool = new TValue[size];
-    }
-
-    public Lease Rent()
-    {
-        if (available == 0)
-        {
-            return new(factory(), this);
-        }
-
-        lock (pool)
-        {
-            if (available == 0)
-            {
-                return new(factory(), this);
-            }
-            available -= 1;
-            var toRent = pool[available];
-            return new(toRent, this);
-        }
-    }
-
-    void Return(TValue toReturn)
-    {
-        if (available == pool.Length - 1)
-        {
-            return;
-        }
-        lock (pool)
-        {
-            if (available == pool.Length - 1)
-            {
-                return;
-            }
-            pool[available] = toReturn;
-            available += 1;
         }
     }
 }
---
+++ Core01/Collections/Pool Pesimistic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace MarcinGajda.Collections;
+class LockingPool<TValue>
+{
+    public struct Lease : IDisposable
+    {
+        readonly TValue value;
+        public TValue Value => isDisposed == AfterDispose
+            ? throw new ObjectDisposedException(nameof(Lease))
+            : value;
+
+        readonly LockingPool<TValue> parent;
+        const int AfterDispose = 1;
+        int isDisposed;
+
+        public Lease(TValue value, LockingPool<TValue> parent)
+        {
+            this.value = value;
+            this.parent = parent;
+        }
+
+        public void Dispose()
+        {
+            var temporary = Interlocked.Exchange(ref isDisposed, AfterDispose);
+            if (temporary != AfterDispose)
+            {
+                parent.Return(value);
+            }
+        }
+    }
+
+    readonly Func<TValue> factory;
+    readonly TValue[] pool;
+    volatile int available;
+
+    public LockingPool(int size, Func<TValue> factory)
+    {
+        this.factory = factory;
+        pool = new TValue[size];
+    }
+
+    bool IsPoolEmpty()
+        => available == 0;
+
+    public Lease Rent()
+    {
+        if (IsPoolEmpty())
+        {
+            return new(factory(), this);
+        }
+        lock (pool)
+        {
+            if (IsPoolEmpty())
+            {
+                return new(factory(), this);
+            }
+            available -= 1;
+            var toRent = pool[available];
+            return new(toRent, this);
+        }
+    }
+
+    bool IsPoolFull()
+        => available == pool.Length;
+
+    void Return(TValue toReturn)
+    {
+        if (IsPoolFull())
+        {
+            return;
+        }
+        lock (pool)
+        {
+            if (IsPoolFull())
+            {
+                return;
+            }
+            pool[available] = toReturn;
+            available += 1;
+        }
+    }
+}
