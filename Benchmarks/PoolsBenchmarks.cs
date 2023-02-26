0760b2c4c5ed9584a55e2c41225774083581a6d2
kirgo36@gmail.com <kirgo36@gmail.com>
2023-02-26 12:48:51 +0100

    more benchmarks

--- Benchmarks/PoolsBenchmarks.cs
+++ Benchmarks/PoolsBenchmarks.cs
@@ -11,45 +11,77 @@ namespace Benchmarks;
 [RankColumn]
 public class PoolsBenchmarks
 {
-    class Random
+    class RandomType { public int X { get; set; } }
+    readonly Func<RandomType> createRandomType = () => new RandomType { X = 5 };
+
+    record ThreadParam<TPool, TLease>(TPool Pool, Func<TPool, TLease> Rent, int N)
+        where TLease : struct, IDisposable;
+
+    [Params(100_000, 1_000_000)]
+    public int Rents { get; set; }
+
+    [Params(1, 2, 8, 128)]
+    public int NumberOfThreads { get; set; }
+
+    Thread[]? threads;
+
+    [IterationSetup(Target = nameof(ThreadStatic))]
+    public void SetupThreadStatic()
     {
-        public int X { get; set; }
+        Setup<ThreadStaticPool<RandomType>, ThreadStaticPool<RandomType>.Lease>();
     }
 
-    [Params(100, 1000)]
-    public int N;
+    [IterationSetup(Target = nameof(Locking))]
+    public void SetupLocking()
+    {
+        Setup<LockingPool<RandomType>, LockingPool<RandomType>.Lease>();
+    }
+
+    public void Setup<TPool, TLease>()
+        where TLease : struct, IDisposable
+    {
+        threads = new Thread[NumberOfThreads];
+        for (int index = 0; index < NumberOfThreads; index++)
+        {
+            var thread = new Thread(pool =>
+            {
+                var typedPool = (ThreadParam<TPool, TLease>)pool!;
+                for (int i = 0; i < typedPool.N; i++)
+                {
+                    using var lease = typedPool.Rent(typedPool.Pool);
+                }
+            });
+            threads[index] = thread;
+        }
+    }
 
     public void Test<TPool, TLease>(TPool pool, Func<TPool, TLease> rent)
         where TLease : struct, IDisposable
     {
-        for (int i = 0; i < N; i++)
+        var threadParam = new ThreadParam<TPool, TLease>(pool, rent, Rents);
+        for (int index = 0; index < threads!.Length; index++)
         {
-            using var lease = rent(pool);
+            threads[index].Start(threadParam);
         }
+
+        Array.ForEach(threads, thread => thread.Join());
     }
 
     [Benchmark]
     public void ThreadStatic()
     {
-        var pool = new ThreadStaticPool<Random>(() => new Random { X = 0 });
+        var pool = new ThreadStaticPool<RandomType>(createRandomType);
         Test(pool, static pool => pool.Rent());
     }
 
     //[Benchmark]
     //public void Spinning()
-    //{
-    //    var pool = new SpiningPool<Random>(64, () => new Random { X = 0 }); // Looks like it deadlocks 
-    //    Parallel.For(0, 1000, _ =>
-    //    {
-    //        using var lease = pool.Rent();
-    //        lease.Value.X += 1;
-    //    });
-    //}
+
 
     [Benchmark]
     public void Locking()
     {
-        var pool = new LockingPool<Random>(64, () => new Random { X = 0 });
+        var pool = new LockingPool<RandomType>(64, createRandomType);
         Test(pool, static pool => pool.Rent());
     }
 }
