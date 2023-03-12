67aae09f99fb7984acc4e6965180eb99a52a89d0
kirgo36@gmail.com <kirgo36@gmail.com>
2023-03-12 13:32:50 +0100

    starting pool everything

---
+++ Core01/Synchronizers/Pooling/PoolEveryting.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MarcinGajda.Synchronizers.Pooling;
+public class Pool<TKey, TPoolInstance>
+{
+    public TResult Use<TKey, TPoolInstance, TResult>(TKey key, Func<TKey, TPoolInstance, TResult> use)
+        => throw null;
+}
