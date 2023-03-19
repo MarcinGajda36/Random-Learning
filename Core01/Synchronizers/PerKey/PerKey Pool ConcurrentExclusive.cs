370a94d507e6d66a2e7e6128640a7d4316246dd5
kirgo36@gmail.com <kirgo36@gmail.com>
2023-03-19 11:10:08 +0100

    trying non-disposable pool and using it for ConcurrentExclusiveSynchronizer

---
+++ Core01/Synchronizers/PerKey/PerKey Pool ConcurrentExclusive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MarcinGajda.Synchronizers.Pooling;
+
+namespace MarcinGajda.Synchronizers;
+
+public sealed partial class PoolPerKeyConcurrentExclusive<TKey>
+    where TKey : notnull
+{
+    public static PowerOfTwo DefaultSize { get; } = new PowerOfTwo(32);
+    readonly PerKeyPool<TKey, ConcurrentExclusiveSynchronizer> pool;
+
+    public PoolPerKeyConcurrentExclusive()
+        : this(DefaultSize) { }
+
+    public PoolPerKeyConcurrentExclusive(PowerOfTwo poolSize)
+        => pool = new(DefaultSize, () => new ConcurrentExclusiveSynchronizer());
+
+    public Task<TResult> ConcurrentAsync<TResult>(
+        TKey key,
+        Func<CancellationToken, Task<TResult>> resultFactory,
+        CancellationToken cancellationToken = default)
+        => pool.Get(key).ConcurrentAsync(resultFactory, cancellationToken);
+
+    public Task<TResult> ExclusiveAsync<TResult>(
+        TKey key,
+        Func<CancellationToken, Task<TResult>> resultFactory,
+        CancellationToken cancellationToken = default)
+        => pool.Get(key).ExclusiveAsync(resultFactory, cancellationToken);
+}
---
+++ Core01/Synchronizers/Pooling/Pool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Numerics;
+
+namespace MarcinGajda.Synchronizers.Pooling;
+
+public sealed class PerKeyPool<TKey, TInsance>
+    where TKey : notnull
+{
+    public static PowerOfTwo DefaultSize { get; } = new PowerOfTwo(32);
+    private readonly ImmutableArray<TInsance> pool;
+    private readonly int poolIndexBitShift;
+
+    public PerKeyPool(Func<TInsance> factory)
+        : this(DefaultSize, factory) { }
+
+    public PerKeyPool(PowerOfTwo poolSize, Func<TInsance> factory)
+    {
+        if (poolSize.Value < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(poolSize), poolSize, "Pool size has to be bigger then 0.");
+        }
+
+        var poolBuilder = ImmutableArray.CreateBuilder<TInsance>((int)poolSize.Value);
+        for (int index = 0; index < pool.Length; index++)
+        {
+            poolBuilder.Add(factory());
+        }
+        pool = poolBuilder.MoveToImmutable();
+        poolIndexBitShift = (sizeof(int) * 8) - BitOperations.TrailingZeroCount(pool.Length);
+    }
+
+    public TInsance Get(TKey key)
+        => pool[GetIndex(key)];
+
+    private int GetIndex(TKey key)
+        => (int)(Hashing.Fibonacci(key) >> poolIndexBitShift);
+}
