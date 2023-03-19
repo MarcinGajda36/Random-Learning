06e22151f6fb7a04ffd883c48e49c13e53f628e8
kirgo36@gmail.com <kirgo36@gmail.com>
2023-03-19 16:46:52 +0100

    some work on MyThreadPoolScheduler

--- Core01/Synchronization/Scheduling/MyThreadPoolScheduler.cs
+++ Core01/Synchronization/Scheduling/MyThreadPoolScheduler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,36 +6,27 @@ using System.Threading.Tasks;
 namespace MarcinGajda.Synchronization.Scheduling;
 internal sealed class MyThreadPoolScheduler : TaskScheduler
 {
+    const int MaxThreads = 4; // 2,4,8?
     const int QueueSize = 32_768;
     const uint WrapAroundMask = QueueSize - 1;
+    const uint CountMask = WrapAroundMask;
+    const uint EnqueueIndexMask = ~CountMask;
+
+    readonly int enqueueIndexBitShift = BitOperations.TrailingZeroCount(QueueSize);
     readonly Task?[] queue = new Task?[QueueSize];
-    readonly Thread[] threads;
+    readonly Thread[] threads = new Thread[MaxThreads];
 
-
-    readonly int enqueueIndexBitShift;
     uint queueInfo;
-    uint QueueInfo => Volatile.Read(ref queueInfo);
-    uint Count => QueueInfo & WrapAroundMask;
-    uint EnqueueIndex => (QueueInfo & ~WrapAroundMask) >> enqueueIndexBitShift;
+    uint Count => queueInfo & CountMask;
+    uint EnqueueIndex => (queueInfo & EnqueueIndexMask) >> enqueueIndexBitShift;
 
-    public MyThreadPoolScheduler(int threadCount = 4)
+    public MyThreadPoolScheduler()
     {
-        if (threadCount < 1)
-        {
-            throw new ArgumentOutOfRangeException(nameof(threadCount), threadCount, "Thread count has to be bigger then 0.");
-        }
-
-        enqueueIndexBitShift = BitOperations.TrailingZeroCount(QueueSize);
-        var perThread = QueueSize / threadCount;
-        threads = new Thread[threadCount];
-        for (int index = 0; index < threadCount; index++)
+        for (int index = 0; index < threads.Length; index++)
         {
             var thread = new Thread(worker => ((Worker)worker!).Work());
             threads[index] = thread;
-            thread.Start(new Worker(
-                this,
-                index * perThread, // TODO Can i have them start at 0..4 and have 4 index step?
-                (index + 1) * perThread));
+            thread.Start(new Worker(this, index, MaxThreads));
         }
     }
 
@@ -70,25 +60,29 @@ internal sealed class MyThreadPoolScheduler : TaskScheduler
     protected override IEnumerable<Task>? GetScheduledTasks()
         => null;
 
+    public override int MaximumConcurrencyLevel
+        => threads.Length;
+
     class Worker
     {
         private readonly MyThreadPoolScheduler parent;
         private readonly int start;
-        private readonly int length;
+        private readonly int step;
 
-        public Worker(MyThreadPoolScheduler parent, int start, int length)
+        public Worker(MyThreadPoolScheduler parent, int start, int step)
         {
             this.parent = parent;
             this.start = start;
-            this.length = length;
+            this.step = step;
         }
 
         internal void Work()
         {
-            var queueWindow = parent.queue.AsSpan(start, length);
-            while (parent.Count > 0)
+            var index = start;
+            var queue = parent.queue;
+            while (parent.Count > 0 && index <= queue.Length)
             {
-                for (int i = 0; i < queueWindow.Length; i++)
+                for (int i = start; i < queue.Length; i += step)
                 {
                     // First worker can be optimized to just try take right? maybe not
                     //if (Interlocked.Exchange(ref ) // TODO
