ffb676c310d2c3db1891c5dd5a1a77ba60ceb81e
kirgo36@gmail.com <kirgo36@gmail.com>
2023-03-23 18:52:11 +0100

    removing leftovers

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -6,9 +6,6 @@ internal class Program
 {
     public static Task Main(string[] args)
     {
-        //var s = new HistoricalToLiveBenchmark_Halves();
-        //s.ElementsCount = 1000;
-        //await s.HistoricalToLive2_Mutable();
         BenchmarkRunner.Run<HistoricalToLiveBenchmark_Halves>();
         return Task.CompletedTask;
     }
