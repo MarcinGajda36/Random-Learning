26286a53ea97473c9ae274848e7d36beba6f7599
kirgo36@gmail.com <kirgo36@gmail.com>
2023-03-31 18:45:15 +0200

    StatefullAsyncEnumerable

---
+++ Core01/RX_IX_Tests/StatefullAsyncEnumerable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace MarcinGajda.RX_IX_Tests;
+
+internal class StatefullAsyncEnumerable
+{
+    public record Message(int Value);
+    public record AddMessage(int Value) : Message(Value);
+    public record RemoveMessage(int Value) : Message(Value);
+
+    public ImmutableArray<int> State1 { get; set; } = ImmutableArray<int>.Empty;
+    public ImmutableDictionary<int, string> State2 { get; set; } = ImmutableDictionary<int, string>.Empty;
+
+
+    private async Task<ImmutableDictionary<int, string>> SomeAsyncStaff(Message message)
+    {
+        return message switch
+        {
+            AddMessage(var value) => State2.Add(value, await Task.FromResult(value.ToString())),
+            RemoveMessage(var value) => State2.Remove(value),
+            var unknown => throw new NotSupportedException($"Unknown {unknown}"),
+        };
+    }
+
+    public async IAsyncEnumerable<int> GetEnumerable(IAsyncEnumerable<Message> messages)
+    {
+        await foreach (var message in messages)
+        {
+            State1 = message switch
+            {
+                AddMessage(var value) => State1.Add(value),
+                RemoveMessage(var value) => State1.Remove(value),
+                var unknown => throw new NotSupportedException($"Unknown {unknown}"),
+            };
+            State2 = await SomeAsyncStaff(message);
+
+            if (messages is AddMessage)
+            {
+                // Now we know that when this message returns then State1 and State2 are up to date
+                yield return message.Value;
+            }
+        }
+    }
+}
