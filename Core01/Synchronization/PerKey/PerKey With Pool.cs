69e86b44c9a8c7a3387f0107389cafaf2f326101
kirgo36@gmail.com <kirgo36@gmail.com>
2023-04-02 08:44:58 +0200

    more PerKeyWithPool testing and comments

--- Core01/Synchronization/PerKey/PerKey With Pool.cs
+++ Core01/Synchronization/PerKey/PerKey With Pool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using MarcinGajda.Synchronizers;
 using MarcinGajda.Synchronizers.Pooling;
@@ -8,18 +9,44 @@ namespace MarcinGajda.Synchronization.PerKey;
 internal class PerKeyWithPool
 {
     private readonly static PowerOfTwo PoolSize = new(32);
-    private readonly PoolPerKeySynchronizerPerf<Guid> synchronizer = new(PoolSize);
+    // Index per pool could be calculated once hmm
+    // Alternative is 1 shared pool with everything
+    private readonly PerKeyPool<Guid, SemaphoreSlim> semaphorePool = new(PoolSize, () => new(1, 1));
     private readonly PerKeyPool<Guid, Queue<int>> queuePool = new(PoolSize, () => new());
+    private readonly PerKeyPool<Guid, List<int>> listPool = new(PoolSize, () => new());
 
-    public Task<int> Caller1(Guid id)
+    public async Task<int> Caller1(Guid id)
     {
-        var queue = queuePool.Get(id);
-        return synchronizer.SynchronizeAsync(id, queue, async (queue, cancellationToken) =>
+        var semaphore = semaphorePool.Get(id);
+        await semaphore.WaitAsync();
+        try
         {
+            var queue = queuePool.Get(id);
             var result = await SomeRestrictedMethod();
             queue.Enqueue(result);
             return result;
-        });
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    public async Task<int> Caller2(Guid id)
+    {
+        var semaphore = semaphorePool.Get(id);
+        await semaphore.WaitAsync();
+        try
+        {
+            var list = listPool.Get(id);
+            var result = await SomeRestrictedMethod();
+            list.Remove(result);
+            return result;
+        }
+        finally
+        {
+            semaphore.Release();
+        }
     }
 
     private Task<int> SomeRestrictedMethod()
