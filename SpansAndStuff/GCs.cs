c36423b2c5fdd8c0a163c09a08d69ad3af8f4745
kirgo36@gmail.com <kirgo36@gmail.com>
2023-04-09 10:19:31 +0200

    GCs stuff

---
+++ SpansAndStuff/GCs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime;
+
+namespace SpansAndStuff;
+internal class GCs
+{
+    public static void NoGCRegionTests()
+    {
+        GC.TryStartNoGCRegion(1, 2, false);
+        // Do work
+        if (GCSettings.LatencyMode == GCLatencyMode.NoGCRegion)
+            GC.EndNoGCRegion();
+    }
+}
