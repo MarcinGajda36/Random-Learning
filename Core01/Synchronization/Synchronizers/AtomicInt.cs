1bbf5b70b41b9f0714207c5ea5fb0101d8f72aea
kirgo36@gmail.com <kirgo36@gmail.com>
2023-07-29 19:33:01 +0200

    AtomOptymistic

--- Core01/Synchronization/Synchronizers/AtomicInt.cs
+++ Core01/Synchronization/Synchronizers/AtomicInt.cs
@@ -1,29 +1,63 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MarcinGajda.Synchronization.Synchronizers;
-public sealed class OptymisticAtomicInt
+public sealed class AtomOptymistic<TValue>
+    where TValue : class
 {
-    private int value;
-    public int Value => value.VolatileRead();
-    public OptymisticAtomicInt(int initial = 0)
+    private TValue value;
+    public TValue Value => Volatile.Read(ref value);
+    public AtomOptymistic(TValue initial)
         => value = initial;
 
-    public int Swap<TArgument>(TArgument argument, Func<int, TArgument, int> swapper)
+    public TValue Map<TArgument>(TArgument argument, Func<TValue, TArgument, TValue> mapper)
     {
         while (true)
         {
-            int initial = Value;
-            int swapped = swapper(initial, argument);
-            if (Interlocked.CompareExchange(ref value, swapped, initial) == initial)
+            var initial = Value;
+            var mapped = mapper(initial, argument);
+            if (Interlocked.CompareExchange(ref value, mapped, initial) == initial)
             {
-                return swapped;
+                return mapped;
             }
         }
     }
 
-    public int Swap(Func<int, int> swapper)
-        => Swap(swapper, static (initial, func) => func(initial));
+    public TValue Map(Func<TValue, TValue> mapper)
+        => Map(mapper, static (initial, func) => func(initial));
+
+    public async Task<TValue> MapAsync<TArgument>(TArgument argument, Func<TValue, TArgument, Task<TValue>> mapper)
+    {
+        while (true)
+        {
+            var initial = Value;
+            var mapped = await mapper(initial, argument);
+            if (Interlocked.CompareExchange(ref value, mapped, initial) == initial)
+            {
+                return mapped;
+            }
+        }
+    }
+
+    public Task<TValue> MapAsync(Func<TValue, Task<TValue>> mapper)
+        => MapAsync(mapper, static (initial, func) => func(initial));
+
+    public async ValueTask<TValue> MapValueAsync<TArgument>(TArgument argument, Func<TValue, TArgument, ValueTask<TValue>> mapper)
+    {
+        while (true)
+        {
+            var initial = Value;
+            var mapped = await mapper(initial, argument);
+            if (Interlocked.CompareExchange(ref value, mapped, initial) == initial)
+            {
+                return mapped;
+            }
+        }
+    }
+
+    public ValueTask<TValue> MapValueAsync(Func<TValue, ValueTask<TValue>> mapper)
+        => MapValueAsync(mapper, static (initial, func) => func(initial));
 }
 
 public static class AtomicInt
