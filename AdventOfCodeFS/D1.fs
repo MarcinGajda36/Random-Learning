daafaf8cbfe83556c806fc5327247f1ed59a3ddc
Marcin Gajda <kirgo36@gmail.com>
2023-12-05 19:12:31 +0100

    Advent of code d1 part 1

---
+++ AdventOfCodeFS/D1.fs
@@ -0,0 +1,16 @@
+﻿module D1
+open System.IO
+open System
+
+let inputPath = Path.Join [| Environment.CurrentDirectory; "D1Input.txt" |]
+let input = File.ReadAllLines inputPath
+
+let firstNumer text = text |> Seq.find Char.IsDigit
+let lastNumber text = text |> Seq.rev |> Seq.find Char.IsDigit
+let fullNumber text = 
+    let first = firstNumer text 
+    let last = lastNumber text
+    (string first) + (string last) 
+    |> int
+    
+let sum = input |> Array.map fullNumber |> Array.sum
