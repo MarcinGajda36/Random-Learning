42cce7d34ac4eafa3322de83f0eb9c73f9577f37
Marcin Gajda <kirgo36@gmail.com>
2024-01-16 22:47:32 +0100

    testing 2 times same interface

--- Core01/ContractsT/ContractsTests.cs
+++ Core01/ContractsT/ContractsTests.cs
@@ -2,8 +2,22 @@
 
 namespace MarcinGajda.ContractsT
 {
+
+    public interface ITest<T>
+    {
+        public T Value { get; }
+    }
+
+    public class Test : ITest<int>, ITest<string>
+    {
+        public int Value => throw new System.NotImplementedException();
+
+        string ITest<string>.Value => throw new System.NotImplementedException();
+    }
+
     public class ContractsTests
     {
+
         public static int Test(int value)
         {
             Contract.Requires(value > 0, "value can't be let then 0");
