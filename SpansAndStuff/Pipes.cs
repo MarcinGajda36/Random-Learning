c150608dc8e2683de49fca28393e209c4dbed28a
Marcin Gajda <kirgo36@gmail.com>
2024-07-02 20:35:00 +0200

    random pipe

---
+++ SpansAndStuff/Pipes.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace SpansAndStuff;
+internal class Pipes
+{
+    public static async Task Test()
+    {
+        var pipe = new Pipe(new PipeOptions() { });
+        PipeReader reader = pipe.Reader;
+        PipeWriter writer = pipe.Writer;
+
+    }
+}
