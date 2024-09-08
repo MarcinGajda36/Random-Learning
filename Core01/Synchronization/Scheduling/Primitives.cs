bd72904bfd26b13e87069be5f7388a982f41bef0
Marcin Gajda <kirgo36@gmail.com>
2024-09-08 20:52:06 +0200

    idk some barier reads?

---
+++ Core01/Synchronization/Scheduling/Primitives.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarcinGajda.Synchronization.Scheduling;
+internal class Primitives
+{
+    long x;
+    public Task Test()
+    {
+        Interlocked.MemoryBarrier();
+        var _x1 = Interlocked.Read(ref x); // Is this stronger then Volatile.Read(...)?
+        var _x2 = Volatile.Read(ref x);
+        return Task.CompletedTask;
+    }
+}
