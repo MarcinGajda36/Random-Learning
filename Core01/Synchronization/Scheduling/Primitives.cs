bd77771e99dbf23aef35c5e937ff8214594dc3bb
Marcin Gajda <kirgo36@gmail.com>
2024-09-08 21:26:31 +0200

    more random bariers

--- Core01/Synchronization/Scheduling/Primitives.cs
+++ Core01/Synchronization/Scheduling/Primitives.cs
@@ -7,6 +7,7 @@ internal class Primitives
     long x;
     public Task Test()
     {
+        Thread.MemoryBarrier(); // This one delegates to Interlocked.MemoryBarrier();
         Interlocked.MemoryBarrier();
         var _x1 = Interlocked.Read(ref x); // Is this stronger then Volatile.Read(...)?
         var _x2 = Volatile.Read(ref x);
