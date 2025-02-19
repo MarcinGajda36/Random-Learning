22cd90d2c0e7d83dfc1575df17e12e5c2cfee552
Marcin Gajda <kirgo36@gmail.com>
2025-02-19 11:13:45 +0100

    adding experimental RangeExtraction.AppendRangeV2(...)

--- LeetCode/RangeExtraction.cs
+++ LeetCode/RangeExtraction.cs
@@ -1,6 +1,7 @@
 ﻿namespace LeetCode;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 public class RangeExtraction
@@ -58,5 +59,39 @@ public class RangeExtraction
             [var one, var two] => destination.Append($"{one},{two}"),
             [var first, .., var last] => destination.Append($"{first}-{last}"),
         };
+    }
+
+    private const char Semicolon = ',';
+    private readonly static CompositeFormat RangeOfOne = CompositeFormat.Parse("{0}");
+    private readonly static CompositeFormat RangeOfTwo = CompositeFormat.Parse("{0},{1}");
+    private readonly static CompositeFormat RangeOfThreeOrMore = CompositeFormat.Parse("{0}-{1}");
+    private readonly static CultureInfo Invariant = CultureInfo.InvariantCulture;
+    private static StringBuilder AppendRangeV2(StringBuilder destination, List<int> range)
+    {
+        // Feels like the worst of all worlds, but it was cool as exercise
+        // Why does it feels so bad?
+        //  - duplicated list patterns
+        //  - a lot of context: fields for CompositeFormat and methods for AppendFormat in contrast to inlined interpolation
+        //  - when adding new Range we need to add 2 cases
+        return (destination, range) switch
+        {
+            (_, []) => destination,
+            ({ Length: 0 }, [var one]) => AppendRangeOfOne(destination, one),
+            ({ Length: 0 }, [var one, var two]) => AppendRangeOfTwo(destination, one, two),
+            ({ Length: 0 }, [var first, .., var last]) => AppendRangeOfThreeOrMore(destination, first, last),
+
+            ({ Length: > 0 }, [var one]) => AppendRangeOfOne(destination.Append(Semicolon), one),
+            ({ Length: > 0 }, [var one, var two]) => AppendRangeOfTwo(destination.Append(Semicolon), one, two),
+            ({ Length: > 0 }, [var first, .., var last]) => AppendRangeOfThreeOrMore(destination.Append(Semicolon), first, last),
+        };
+
+        static StringBuilder AppendRangeOfOne(StringBuilder destination, int one)
+            => destination.AppendFormat(Invariant, RangeOfOne, one);
+
+        static StringBuilder AppendRangeOfTwo(StringBuilder destination, int one, int two)
+            => destination.AppendFormat(Invariant, RangeOfTwo, one, two);
+
+        static StringBuilder AppendRangeOfThreeOrMore(StringBuilder destination, int first, int last)
+            => destination.AppendFormat(Invariant, RangeOfThreeOrMore, first, last);
     }
 }
