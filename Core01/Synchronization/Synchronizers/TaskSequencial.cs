705dfdd48fcb6fca7714bbfd4ee0279d3e2a5c64
Marcin Gajda <kirgo36@gmail.com>
2025-03-21 22:35:35 +0100

    TaskSequencial AddNextGlobal

---
+++ Core01/Synchronization/Synchronizers/TaskSequencial.cs
@@ -0,0 +1,77 @@
+﻿namespace MarcinGajda.Synchronization.Synchronizers;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+public static class TaskSequencial
+{
+    private interface IOperation
+    {
+        Task ExecuteAsync();
+    }
+
+    private sealed class Operation<TResult> : IOperation, IAsyncDisposable
+    {
+        private readonly Func<CancellationToken, Task<TResult>> operation;
+        private readonly TaskCompletionSource<TResult> completionSource = new();
+        private readonly CancellationToken cancellationToken;
+        private readonly CancellationTokenRegistration taskCancellation;
+
+        public Task<TResult> Task
+            => completionSource.Task;
+
+        public Operation(Func<CancellationToken, Task<TResult>> operation, CancellationToken cancellationToken)
+        {
+            this.operation = operation;
+            this.cancellationToken = cancellationToken;
+            taskCancellation = cancellationToken.Register(
+                static @this =>
+                {
+                    var operation = (Operation<TResult>)@this!;
+                    _ = operation.completionSource.TrySetCanceled(operation.cancellationToken);
+                },
+                this);
+        }
+
+        public async Task ExecuteAsync()
+        {
+            var completionSource_ = completionSource;
+            try
+            {
+                var cancellationToken_ = cancellationToken;
+                _ = cancellationToken_.IsCancellationRequested
+                    ? completionSource_.TrySetCanceled(cancellationToken_)
+                    : completionSource_.TrySetResult(await operation(cancellationToken_));
+            }
+            catch (Exception exception)
+            {
+                _ = completionSource_.TrySetException(exception);
+            }
+        }
+
+        public ValueTask DisposeAsync()
+            => taskCancellation.DisposeAsync();
+    }
+
+    private static readonly ActionBlock<IOperation> globalSequence
+        = new(static operation => operation.ExecuteAsync());
+
+    public static async Task<TResult> AddNextGlobal<TResult>(
+        Func<CancellationToken, Task<TResult>> toRun,
+        CancellationToken cancellationToken)
+    {
+        await using var operation = new Operation<TResult>(toRun, cancellationToken);
+        _ = globalSequence.Post(operation);
+        return await operation.Task;
+    }
+
+    public static Task<TResult> WhenAll<TResult>(
+        IEnumerable<Func<CancellationToken, Task<TResult>>> funcs,
+        CancellationToken cancellationToken)
+    {
+
+        return null;
+    }
+}
