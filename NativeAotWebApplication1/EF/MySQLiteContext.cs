da28bdbd63443a43f86fd1345aaa6711a6a08e9a
Marcin Gajda <kirgo36@gmail.com>
2025-03-27 13:18:51 +0100

    New project to take a look at AOT Web Application and de-rust some EF

---
+++ NativeAotWebApplication1/EF/MySQLiteContext.cs
@@ -0,0 +1,50 @@
+﻿namespace NativeAotWebApplication1.EF;
+
+using Microsoft.EntityFrameworkCore;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string lastName { get; set; }
+
+    public ICollection<Address> Addresses { get; set; } = new List<Address>();
+}
+
+public class Address
+{
+    public int Id { get; set; }
+    public string SomethingAddress { get; set; }
+
+    public int PersonId { get; set; }
+    public Person? Person { get; set; }
+}
+
+public class MySQLiteContext : DbContext
+{
+    public DbSet<Person> People { get; }
+    public DbSet<Address> Addresses { get; }
+
+    public MySQLiteContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>();
+        modelBuilder.Entity<Address>(addresses =>
+        {
+            addresses
+                .HasOne<Person>()
+                .WithMany(person => person.Addresses)
+                .HasForeignKey(address => address.PersonId);
+        });
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(); //TODO <- Connection string could go here, or in appsettings
+        base.OnConfiguring(optionsBuilder);
+    }
+}
