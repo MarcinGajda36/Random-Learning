695ea528b26ef0a58597c12266fc21fc0f34d370
Marcin Gajda <kirgo36@gmail.com>
2025-04-14 16:41:40 +0200

    fixing orleans learning projects

--- OrleansHelloWorld/ProgramSilo.cs
+++ OrleansHelloWorld/ProgramSilo.cs
@@ -1,8 +1,12 @@
 ﻿namespace OrleansSilo01;
 
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using Grains.GrainStorage;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Orleans.Hosting;
 
 internal class ProgramSilo
 {
@@ -13,14 +17,14 @@ internal class ProgramSilo
             .UseOrleans(silo
                 => silo
                     .UseLocalhostClustering()
-                    .ConfigureLogging(logging => logging.AddConsole()))
-                    //.AddFileGrainStorage("File", options =>
-                    //{
-                    //    string path = Environment.GetFolderPath(
-                    //        Environment.SpecialFolder.ApplicationData);
+                    .ConfigureLogging(logging => logging.AddConsole())
+                    .AddFileGrainStorage("File", options =>
+                    {
+                        string path = Environment.GetFolderPath(
+                            Environment.SpecialFolder.ApplicationData);
 
-                    //    options.RootDirectory = Path.Combine(path, "Orleans/GrainState/v1");
-                    //})
+                        options.RootDirectory = Path.Combine(path, "Orleans/GrainState/v1");
+                    }))
             .UseConsoleLifetime();
 
         using var host = hostBuilder.Build();
