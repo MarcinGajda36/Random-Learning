35298241842ed8f89f9ad1b317ceea167da210c1
Marcin Gajda <kirgo36@gmail.com>
2025-04-30 13:33:48 +0200

    InRef experiments

---
+++ SpansAndStuff/InRef.cs
@@ -0,0 +1,25 @@
+﻿namespace SpansAndStuff;
+internal class InRef
+{
+    public static void TestInTest()
+    {
+        TestIn(5);
+    }
+
+    public static int TestIn(in int x)
+    {
+        return x;
+    }
+
+    public static void TestRefTest()
+    {
+        var five = 5;
+        TestRef(ref five);
+        //TestRef(5); // This doesn't compile
+    }
+
+    public static int TestRef(ref int x)
+    {
+        return x;
+    }
+}
