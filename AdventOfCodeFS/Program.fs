ec4b2daf7ff482ba7bf2688770b446f47615ec7d
Marcin Gajda <kirgo36@gmail.com>
2025-07-02 08:15:25 +0200

    checking out base class vs interface case

--- AdventOfCodeFS/Program.fs
+++ AdventOfCodeFS/Program.fs
@@ -1,2 +1,4 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+{1..2..5}
---
+++ Core01/GenericsTests/InterfaceVsBase.cs
@@ -0,0 +1,23 @@
+﻿namespace MarcinGajda.GenericsTests;
+using System;
+
+// I want adder that concats positive number to the end of string, throws on null string or negative number
+
+// For inheritance i can enforce that all implementation enforce contract.
+public abstract class AdderBase
+{
+    public string Adder(string left, int positiveNumber)
+    {
+        ArgumentNullException.ThrowIfNull(left);
+        ArgumentOutOfRangeException.ThrowIfLessThan(positiveNumber, 0);
+        return AdderCore(left, positiveNumber);
+    }
+
+    protected abstract string AdderCore(string left, int positiveNumber);
+}
+
+// For interface there is no guarantee, only trust and expectation.
+public interface IAdder
+{
+    string Adder(string left, int positiveNumber);
+}
