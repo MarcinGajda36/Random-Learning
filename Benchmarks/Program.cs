631ba8451e376a0aacbbaf4c76e2609ee85c29f5
Marcin Gajda <kirgo36@gmail.com>
2025-09-22 16:06:56 +0200

    comments to remember

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -5,8 +5,11 @@ namespace Benchmarks;
 internal class Program
 {
     //PS C:\Code\Random-Learning\Benchmarks> dotnet run -c Release
+    // run PS as admin
+    // folder: C:\Code\Random-Learning\Benchmarks
+    // example command: dotnet run -c Release -f net8.0
     public static void Main(string[] args)
     {
-        BenchmarkRunner.Run<HistoricalToLiveBenchmark_Halves>();
+        BenchmarkRunner.Run<PoolsBenchmarks>();
     }
 }
