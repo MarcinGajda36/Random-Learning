6b3dd86e6ad3c174b9940880da4c05714f3e736f
Marcin Gajda <kirgo36@gmail.com>
2025-09-26 16:53:14 +0200

    removing old stuff

--- Core01/NewSwitches/NewSwitch.cs
+++ Core01/NewSwitches/NewSwitch.cs
@@ -1,19 +1,12 @@
-﻿using System;
+﻿namespace MarcinGajda.NewSwitches;
+
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Runtime.InteropServices;
-
-namespace MarcinGajda.NewSwitches;
 
 public static class NewSwitch
 {
-    public static int? Test1(IList<int> list) => list switch
-    {
-        null => null,
-        var xs when xs.Any() => xs.First(),
-        _ => null,
-    };
     public static string? Test2(IDictionary<int, string> dictionary, int key) => dictionary switch
     {
         null => null,
