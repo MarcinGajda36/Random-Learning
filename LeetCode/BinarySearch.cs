786d3b3fbb795dc87d16ad60ac7880ef9967a01e
Marcin Gajda <kirgo36@gmail.com>
2026-04-24 22:00:55 +0200

    doodling a binary search

---
+++ LeetCode/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+using System.Collections.Generic;
+
+public class BinarySearch
+{
+    public static int FindIndexOf<TElement>(IReadOnlyList<TElement> haystack, TElement toFind)
+    {
+        if (haystack is not { Count: > 0 })
+            return -1;
+
+        var lowerLimit = 0;
+        var upperLimit = haystack.Count - 1;
+        while (lowerLimit <= upperLimit && upperLimit >= lowerLimit)
+        {
+            var indexToCheck = (upperLimit + lowerLimit) / 2;
+            var candidate = haystack[indexToCheck];
+            switch (Comparer<TElement>.Default.Compare(candidate, toFind))
+            {
+                case > 0:
+                    upperLimit = indexToCheck - 1;
+                    break;
+                case 0:
+                    return indexToCheck;
+                case < 0:
+                    lowerLimit = indexToCheck + 1;
+                    break;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ TestProject1/BinarySearchTests.cs
@@ -0,0 +1,48 @@
+﻿namespace TestProject1;
+
+using LeetCode;
+
+internal class BinarySearchTests
+{
+    [Test]
+    public void BinarySearchTests01()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1, 2, 3, 4, 5], 6), Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void BinarySearchTests02()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1, 2, 3, 4, 5, 6], 7), Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void BinarySearchTests03()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1, 2, 3, 4, 5, 6], 1), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void BinarySearchTests04()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1, 2, 3, 4, 5, 6], 6), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void BinarySearchTests05()
+    {
+        Assert.That(BinarySearch.FindIndexOf([], 1), Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void BinarySearchTests06()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1], 1), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void BinarySearchTests07()
+    {
+        Assert.That(BinarySearch.FindIndexOf([1, 2, 3, 4, 5, 6], 4), Is.EqualTo(3));
+    }
+}
