5611842c97f0cd6845d1c9c52e267bffb68b4dc5
Marcin Gajda <kirgo36@gmail.com>
2026-05-18 12:48:29 +0200

    HistoricalToLive_Forgetting

---
+++ Core01/RX_IX_Tests/HistoricalToLive_Forgeting.cs
@@ -0,0 +1,49 @@
+﻿namespace MarcinGajda.RX_IX_Tests;
+
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+internal class HistoricalToLive_Forgetting
+{
+    public static IObservable<TValue> ConcatLiveAfterHistory<TValue>(
+        IObservable<TValue> live,
+        IObservable<TValue> historical)
+    {
+        var historyChannel = Channel.CreateUnbounded<TValue>();
+        var liveChannel = Channel.CreateUnbounded<TValue>();
+        return Observable.Create<TValue>(async (observer, cancelationToken) =>
+        {
+            using var historySubscription = historical.Subscribe(
+                (value) => historyChannel.Writer.TryWrite(value),
+                (exception) => historyChannel.Writer.TryComplete(exception),
+                () => historyChannel.Writer.TryComplete());
+
+            using var liveSubscription = live.Subscribe(
+                (value) => liveChannel.Writer.TryWrite(value),
+                (exception) => liveChannel.Writer.TryComplete(exception),
+                () => liveChannel.Writer.TryComplete());
+
+            await ConsumeChannel(observer, historyChannel.Reader, cancelationToken);
+            await ConsumeChannel(observer, liveChannel.Reader, cancelationToken);
+        });
+    }
+
+    private static async Task ConsumeChannel<TValue>(IObserver<TValue> observer, ChannelReader<TValue> channel, CancellationToken cancelationToken)
+    {
+        while (await channel.WaitToReadAsync(cancelationToken))
+        {
+            while (channel.TryRead(out var value))
+            {
+                observer.OnNext(value);
+            }
+        }
+        var completion = channel.Completion;
+        if (completion.IsFaulted)
+        {
+            observer.OnError(completion.Exception);
+        }
+    }
+}
