d41056ab91ad2d88ddc07c0b49f4a60af3c69c52
Marcin Gajda <kirgo36@gmail.com>
2026-07-29 12:33:27 +0200

    Normal web api

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,49 @@
+
+namespace WebApplication1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        _ = builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        _ = builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            _ = app.MapOpenApi();
+        }
+
+        _ = app.UseHttpsRedirection();
+
+        _ = app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        _ = app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[Random.Shared.Next(summaries.Length)]
+                })
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
